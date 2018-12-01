8283ac7d869783cff52f48d1f2a73a1e86d9ff9c
2mol <2mol@users.noreply.github.com>
2018-12-01 21:19:27 +0100

    day1part1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,3 +1,17 @@
 module Day01 where
 
-bla = undefined
+main = do
+    input <- readFile "day01input"
+    let drifts = map signedRead (lines input)
+    putStrLn $ "part 1: " <> show (sum drifts)
+
+    let drifts' = drifts <> drifts'
+        bla = undefined
+
+    putStrLn $ "part 2: "
+
+signedRead :: String -> Int
+signedRead s =
+    if head s == '+'
+       then read $ tail s
+       else read s
